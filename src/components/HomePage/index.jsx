c7b3078180f390b98583a44a995e055de92a5252
Balasubramani M <bala.m@live.in>
2020-01-21 19:31:42 +0530

    Filters completed.

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -3,19 +3,43 @@ import Card from "components/Card";
 import API from "utils/API";
 import Skeleton from "utils/Skeleton";
 import Pagination from "components/Pagination";
-import { Section, H4 } from "./styles";
-import { Container, Row, Col } from "react-bootstrap";
+import {
+  Section,
+  H4,
+  SortFilterContainer,
+  Text,
+  FilterContainer,
+  FilterSection,
+} from "./styles";
+import {
+  Container,
+  Row,
+  Col,
+  DropdownButton,
+  Dropdown,
+  InputGroup,
+  FormControl,
+  Button,
+  Modal,
+  Form
+} from "react-bootstrap";
 
 const Dashboard = () => {
   const [data, setData] = useState([]);
+  const [dataClone, setDataClone] = useState([]);
   const [totalPages, setTotalPages] = useState(1);
   const [loading, setLoading] = useState(true);
+  const [sortBy, setSortBy] = useState("Ascending");
+  const [show, setShow] = useState(false);
+  const [species, setSpecies] = useState([]);
+  const [genders, setGenders] = useState([]);
 
   useEffect(() => {
     if (data.length === 0) {
       API.get("/").then(res => {
         const { data: { results = [], info } = {} } = res;
         setData(results);
+        setDataClone(results);
         setTotalPages(info.pages || 1);
         setLoading(false);
       });
@@ -34,16 +58,134 @@ const Dashboard = () => {
     });
   };
 
+  const sortByAsc = () => {
+    setSortBy("Ascending");
+    const sortedData = data.sort((a, b) => a - b);
+    setData(sortedData);
+  };
+
+  const sortByDsc = () => {
+    setSortBy("Descending");
+    const sortedData = data.sort((a, b) => b - a);
+    setData(sortedData);
+  };
+
+  const FilterComponent = () => {
+
+    const getSpecies = apiData => {
+      const speciesList = [...new Set(apiData.map(res => res.species))];
+      return speciesList.map(type => ({ type: type, value: true }));
+    };
+
+    const getGenders = apiData => {
+      const gendersList = [...new Set(apiData.map(res => res.gender))];
+      return gendersList.map(gender => ({ type: gender, value: true }));
+    };
+
+    useEffect(() => {
+      if(species.length === 0) {
+        const speciesArr = getSpecies(dataClone);
+        setSpecies(speciesArr);
+      }
+      if(genders.length === 0) {
+        const gendersArr = getGenders(dataClone);
+        setGenders(gendersArr);
+      }
+    }, [species, genders]);
+    
+
+    const handleSpecies = (index, e) => {
+      const { checked } = e.target;
+      species[index].value = checked;
+      const selectedSpecies = species.filter(res => res.value).map(res => res.type);
+      const filteredData = dataClone.filter(d => selectedSpecies.indexOf(d.species) > -1);
+      setData(filteredData);
+      setSpecies([...species]);
+    };
+
+    const handleGenders = (index, e) => {
+      const { checked } = e.target;
+      genders[index].value = checked;
+      const selectedGenders = genders.filter(res => res.value).map(res => res.type);
+      const filteredData = dataClone.filter(d => selectedGenders.indexOf(d.gender) > -1);
+      setData(filteredData);
+      setGenders([...genders]);
+    };
+
+    return (
+      <Row>
+            <Col>
+              <Text>Filter based on Species</Text>
+              <Form>
+                {species.map((obj, index) => (
+                  <Form.Check
+                    inline
+                    label={obj.type}
+                    key={obj.type}
+                    type="checkbox"
+                    checked={obj.value}
+                    onChange={e => handleSpecies(index, e)}
+                    id={`inline-${obj.type}-1`}
+                  />
+                ))}
+              </Form>
+            </Col>
+            <Col>
+              <Text>Filter based on Gender</Text>
+              <Form>
+                {genders.map((obj, index) => (
+                  <Form.Check
+                    inline
+                    label={obj.type}
+                    key={obj.type}
+                    type="checkbox"
+                    checked={obj.value}
+                    onChange={e => handleGenders(index, e)}
+                    id={`inline-${obj.type}-1`}
+                  />
+                ))}
+              </Form>
+            </Col>
+            </Row>
+    );
+  };
+
   return (
     <Section>
       <Container>
         <Row>
-          <Col xl={6} lg={6} md={12} sm={12}>
+          <Col xl={12} lg={12} md={12} sm={12}>
             <H4>Rick and Morty Characters</H4>
           </Col>
           <Col xl={6} lg={6} md={12} sm={12}></Col>
         </Row>
-
+        <SortFilterContainer>
+          <Row>
+            <Col>
+              <DropdownButton
+                id="dropdown-basic-button"
+                title={`Sort by ID - ${sortBy}`}
+                variant="secondary"
+              >
+                <Dropdown.Item
+                  active={sortBy === "Ascending"}
+                  onClick={sortByAsc}
+                >
+                  Sort by Ascending
+                </Dropdown.Item>
+                <Dropdown.Item
+                  active={sortBy === "Descending"}
+                  onClick={sortByDsc}
+                >
+                  Sort by Descending
+                </Dropdown.Item>
+              </DropdownButton>
+            </Col>
+            <Col>
+              <FilterComponent />
+            </Col>
+          </Row>
+        </SortFilterContainer>
         <Row>
           {loading ? (
             <Skeleton />
--- src/components/HomePage/styles.jsx
+++ src/components/HomePage/styles.jsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export const Section = styled.div`
-  background-image: url('static/images/so-white.png');
+  background-image: url("static/images/so-white.png");
+  background-attachment: fixed;
+  margin-bottom: 48px;
   & > * {
     .spacing-col {
       padding: 12px;
@@ -12,3 +14,15 @@ export const Section = styled.div`
 export const H4 = styled.h3`
   margin: 24px 0px;
 `;
+
+export const SortFilterContainer = styled.div`
+  margin: 12px 0px;
+`;
+
+export const Text = styled.h6`
+  margin: 12px 0px;
+`;
+
+export const FilterContainer = styled.div``;
+
+export const FilterSection = styled.div``;
